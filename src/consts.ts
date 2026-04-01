6ac691d7eac8c1a071f0af3e7f4243b270d4d56a
ai-mr-sultan <cs.mrsultan@gmail.com>
2026-04-02 01:25:31 +0700

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "AI MR SULTAN;
-export const SITE_DESCRIPTION = "";
+export const SITE_DESCRIPTION = "Selamat datang di mr sultan adalah layanan resmi dengan teknologi ai terbaru, menghadirkan sistem cepat, pasaran lengkap, serta pengalaman bermain yang aman dan modern.!";
